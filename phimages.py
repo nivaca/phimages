926bf2b2a1f7069a407518e3db0ebea25377a269
Nicolas Vaughan <nivaca@fastmail.com>
2021-10-03 11:56:21 -0500

    Remove global variables, use a dictionary instead.

--- phimages.py
+++ phimages.py
@@ -1,37 +1,31 @@
 #!/usr/bin/env python3
 """ Renames images and reformats .md according to PH requirements.
  Created by Nicolas Vaughan (nivaca@fastmail.com), 2021. """
+import click
 import os
 import re
 import shutil
 import sys
-import click
-
-# Global variables
-Makebackup = True
-Dryrun = False
-Imgdir = "img/"
-Extensions: list[str] = ['.png', '.jpg', '.jpeg', '.webp', '.gif']
-Inputfile = ""
+from pprint import pprint as pp
 
 
-def get_names_from_dir() -> list:
-    listing = os.listdir(Imgdir)
+def get_names_from_dir(params: dict) -> list:
+    listing = os.listdir(params["imgdir"])
     real_list = []
     if not listing:
-        print(f"Error. Empty images dir {Imgdir}. Aborting.")
+        click.secho(f'Error. Empty images dir {params["imgdir"]}. Aborting.', fg='red')
         sys.exit(1)
     for file in listing:
         file_name, file_extension = os.path.splitext(file)
-        if file_extension in Extensions:
+        if file_extension in params["extensions"]:
             real_list.append(file)
     return real_list
 
 
-def parsefile() -> list:
+def parsefile(params: dict) -> list:
     """ Reads the .md file and extracts all image file names.
     We will assume that an image appears at most once in the md. """
-    with open(Inputfile, "r", encoding="utf-8") as f:
+    with open(params["inputfile"], "r", encoding="utf-8") as f:
         lines = f.readlines()
     md_list = []  # list of list of filenames and line numbers in md file
     pattern = r"""figure.html +filename=['"](.+?)['"]"""
@@ -45,10 +39,10 @@ def parsefile() -> list:
             fn = match.group(1).strip()
 
             if fn in [i[0] for i in md_list]:
-                print(f"!Error: {fn} is already found in {Inputfile}\n"
-                      f"  If the image needs to appear more than once,\n"
-                      f"  please rename each new occurrance both in the document\n"
-                      f"  and in {Imgdir}.")
+                click.secho(f"""!Error: {fn} is already found in {params["inputfile"]}\n
+                            If the image needs to appear more than once,\n
+                            please rename each new occurrance both in the document\n
+                            and in {params["imgdir"]}.""", fg='red')
                 sys.exit(1)
 
             # we append a list of file name and line number
@@ -57,30 +51,31 @@ def parsefile() -> list:
     return md_list
 
 
-def compare_lists_real_to_md(real_list: list, md_list: list) -> bool:
+def compare_lists_real_to_md(params: dict) -> bool:
     identical = True
-    for f in real_list:
-        if f not in md_list:
+    for f in params["real_list"]:
+        if f not in params["full_md_list"]:
             identical = False
-            print(f"Warning: File {f} in {Imgdir} is not referenced in {Inputfile}. Ignoring.")
+            click.secho(f"""Warning: File {f} in {params["imgdir"]} is not referenced in {params["inputfile"]}.\n
+            It will be ignored but you should delete it if not needed.""", fg='yellow')
     return identical
 
 
-def compare_lists_md_to_real(real_list: list, md_list: list) -> bool:
+def compare_lists_md_to_real(params: dict) -> bool:
     identical = True
-    for f in md_list:
-        if f not in real_list:
+    for f in params["full_md_list"]:
+        if f not in params["real_list"]:
             identical = False
-            print(f"Error: Entry for {f} in {Inputfile} has no corresponding file in {Imgdir}")
+            click.secho(f"""Error: Entry for {f} in {params["inputfile"]} 
+            has no corresponding file in {params["imgdir"]}""", fg='red')
     return identical
 
 
-def compare_lists(real_list: list, full_md_list: list):
-    md_list = [i[0] for i in full_md_list]
-    if not compare_lists_md_to_real(real_list, md_list):
-        print("Aborting")
+def compare_lists(params: dict):
+    if not compare_lists_md_to_real(params):
+        click.secho("Aborting", fg='red')
         sys.exit(1)
-    elif not compare_lists_real_to_md(real_list, md_list):
+    elif not compare_lists_real_to_md(params):
         ans = input("Continue? [Y/n] ").lower()
         if ans not in ["y", ""]:
             sys.exit(0)
@@ -89,95 +84,136 @@ def compare_lists(real_list: list, full_md_list: list):
         return True
 
 
-def createbackup(fn: str):
-    if Makebackup:
-        try:
-            shutil.copyfile(fn, f"{fn}.bak")
-        except OSError:
-            print(f"Error: Could not backup {fn}")
+def createbackup(fname: str) -> bool:
+    try:
+        shutil.copyfile(fname, f"{fname}.bak")
+        return True
+    except OSError:
+        click.secho(f"Error: Could not backup {fname}", fg='red')
+        return False
 
 
-def change_names(md_list: list, lesson_name: str) -> bool:
-    if not Dryrun:
-        if Makebackup:
-            createbackup(Inputfile)
-        with open(Inputfile, "r", encoding="utf-8") as f:
+def change_names(params: dict) -> bool:
+    if not params["dryrun"]:
+        if params["mkbkp"]:
+            createbackup(params["inputfile"])
+        with open(params["inputfile"], "r", encoding="utf-8") as f:
             lines = f.readlines()
 
-        image_count = len(md_list)
+        image_count = len(params["full_md_list"])
         for i in range(image_count):
-            full_file_name, ln = md_list[i]
-            fn, fn_ext = os.path.splitext(full_file_name)
+            full_fname, ln = params["md_list"][i]
+            fn, fn_ext = os.path.splitext(full_fname)
             # format: lesson-name01.ext, lesson-name02.ext, etc.
-            new_fn = lesson_name + str(i + 1).zfill(2) + fn_ext
+            new_fname = params["lesson_name"] + str(i + 1).zfill(2) + fn_ext
             lineno = int(ln) - 1  # adjust for 0-index
             oldline = lines[lineno]
-            newline = re.sub(rf"{full_file_name}", rf"{new_fn}", oldline)
+            newline = re.sub(rf"{full_fname}", rf"{new_fname}", oldline)
             lines[lineno] = newline  # change that line in the list
-            print(f"{full_file_name} => {new_fn}")
+            click.secho(f"{full_fname} => {new_fname}", fg='blue')
             # perform file rename in img dir
-            if not Dryrun:
-                rename_file(full_file_name, new_fn)
+            if not params["dryrun"]:
+                rename_file(full_fname, new_fname, params)
 
-        if not Dryrun:
-            with open(Inputfile, "w", encoding="utf-8") as f:
+        if not params["dryrun"]:
+            with open(params["inputfile"], "w", encoding="utf-8") as f:
                 try:
                     f.writelines(lines)
                     return True
                 except IOError:
-                    print("!Error: could not write new file.")
+                    click.secho("!Error: could not write new file.", fg='red')
                     sys.exit(1)
 
 
-def rename_file(old_name: str, new_name: str):
-    if Makebackup:
-        createbackup(Imgdir + old_name)
+def rename_file(full_fname: str, new_fname: str, params: dict):
+    if params["mkbkp"]:
+        createbackup(params["imgdir"] + full_fname)
     try:
-        os.rename(Imgdir + old_name, Imgdir + new_name)
+        os.rename(params["imgdir"] + full_fname, params["imgdir"] + new_fname)
     except IOError:
-        print(f"!Error: could not rename {old_name}.")
+        click.secho(f"Error: could not rename {full_fname}.", fg='red')
         sys.exit(1)
 
 
+def perform_tests(params: dict) -> bool:
+    rescomp = compare_lists(params)
+    if params["checkonly"]:
+        return rescomp and check_names(params)
+    else:
+        # we don't need to check names only when renaming etc.
+        return rescomp
+
+
+def check_names(params: dict) -> bool:
+    filelist = [i[0] for i in params["full_md_list"]]
+    pattern = params["lesson_name"] + r"\d{1,3}\.\w{3,4}"
+    print(f"{pattern=}")
+    result = True
+    errors = 0
+    # format: lesson-name01.ext, lesson-name02.ext, etc.
+    for fn in filelist:
+        if not re.match(pattern, fn):
+            egfname = params["lesson_name"] + "03.png"
+            click.secho(f"Error: {fn} does not comply with pattern required (e.g. {egfname})",
+                        fg='red')
+            errors += 1
+            result = False
+    if not result:
+        if errors > 1:
+            err_noun = "errors"
+        else:
+            err_noun = "error"
+        click.secho(f"{errors} {err_noun} found in file naming pattern.", fg='red')
+    return result
+
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# +                               main()                               +
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 @click.command()
+@click.option('--checkonly', default=False, flag_value=True, help='Only check the files.', show_default=True)
 @click.option('--mkbkp', default=True, flag_value=True, help='Backup all files it changes/renames.', show_default=True)
 @click.option('--dryrun', default=False, flag_value=True, help='Dry run (do not make any changes).', show_default=True)
-@click.option('--imgdir', default=Imgdir, help='Image directory', show_default=True)
+@click.option('--imgdir', default='img/', help='Image directory', show_default=True)
 @click.argument('inputfile', type=click.Path(exists=True))
-def main(inputfile: str, imgdir: str, dryrun: bool, mkbkp: bool):
+def main(inputfile: str, imgdir: str, dryrun: bool, mkbkp: bool, checkonly: bool):
     mdfn, mdfn_ext = os.path.splitext(inputfile)
-    if mdfn_ext.lower() != 'md':
+    if mdfn_ext.lower() != '.md':
         click.secho(f'Error: input file ({inputfile}) must have ".md" extension.', fg='red')
         sys.exit(0)
-    else:
-        global Inputfile
-        Inputfile = inputfile
 
-    global Makebackup
-    if not mkbkp:
-        Makebackup = False
+    params = {
+        "inputfile": inputfile,
+        # "real_list": real_list,
+        # "full_md_list": full_md_list,
+        # "md_list": md_list,
+        "lesson_name": os.path.splitext(inputfile)[0],
+        # "extensions": extensions,
+        "extensions": ['.png', '.jpg', '.jpeg', '.webp', '.gif'],
+        "imgdir": imgdir,
+        "checkonly": checkonly,
+        "dryrun": dryrun,
+        "mkbkp": mkbkp,
+    }
 
-    global Imgdir
-    Imgdir = imgdir
-
-    global Dryrun
     if dryrun:
-        Dryrun = True
+        click.secho("Dry run: no files will be changed", fg='blue')
 
-    # we take the lesson's name from the lesson's base file
-    lesson_name = os.path.splitext(Inputfile)[0]
+    if checkonly:
+        click.secho("Performing tests only.", fg='blue')
 
-    real_list = get_names_from_dir()
+    params["real_list"] = get_names_from_dir(params)
 
     # this dict contains both filename and the list of line no.
     # of the file names in the md
-    md_list = parsefile()
+    params["full_md_list"] = parsefile(params)
+    params["md_list"] = [i[0] for i in params["full_md_list"]]
 
-    if Dryrun:
-        print("Dry run: no files would be changed")
-
-    if compare_lists(real_list, md_list):
-        change_names(md_list, lesson_name)
+    if perform_tests(params):
+        click.secho(f"Image files in {imgdir} and image links in {inputfile} seem to match.", fg="blue")
+        if not checkonly:
+            change_names(params)
 
 
 if __name__ == "__main__":
